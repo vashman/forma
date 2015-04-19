ad003a786bcebec0a0b70c5795b8c1394799443c
Sunny S <3030712@gmail.com>
2015-04-19 01:03:35 -0700

    updated flag to use taxo lib.

--- include/flag.hpp
+++ include/flag.hpp
@@ -9,37 +9,13 @@
 namespace forma {
 /**/
 template <typename charT, typename traits, typename allocator>
-class basic_flag : public basic_tag<charT,traits,allocator> {
-public:
-  basic_flag() = default;
-
-  template <typename InputIt>
-  basic_flag(
-    InputIt _tag_begin
-  , InputIt _tag_end
-  , InputIt _root_end
-  , InputIt _value_end
-  )
-  : basic_tag<charT,traits,allocator> (_tag_begin, _tag_end, _tag_end, _root_end)
-  , value (_root_end, _value_end) {
-  }
-
-  bool
-  operator<(
-    basic_flag<charT,traits,allocator> const &
-  ) const;
-
-  std::basic_string<charT,traits,allocator> value;
-};
-
-/* operator < */
-template <typename charT, typename traits, typename allocator>
-bool
-basic_flag<charT,traits,allocator>::operator<(
-  basic_flag<charT,traits,allocator> const & _flag
-) const {
-return ((_flag.value < this->value) && (_flag < *this));
-}
+using basic_flag
+  = taxo::realtionship <
+      taxo::catagory<charT,traits,allocator>
+    , charT
+    , traits
+    , allocator
+  >;
 
 } /* forma */
 #endif
