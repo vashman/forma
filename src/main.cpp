317db74a4d7913dc642e56e5ac71cb8e4a4c543d
Sunny S <3030712@gmail.com>
2016-06-07 16:32:02 -0700

    updates
    - updated to use new data model interface and iterators.

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,17 @@
 #include <set>
 #include <vector>
 #include "../include/forma.hpp"
-#include <data_pattern/iterator.hpp>
-
-#include <data_pattern/sqlite.hpp>
-#include "tag_iwriter.cpp"
+#include <data_pattern/cast_iterator.hpp>
 
 using std::back_inserter;
 using std::vector;
-using std::set;
 using std::copy;
-using data_pattern::data_model;
-using
-data_pattern::idata_model_iterator;
+using iterator_decorator::cast_iterator;
+
 using forma::tag_t;
-using forma::context_t;
-
-data_model *
-make_context();
-
-data_model *
-make_formadb();
-
-void
-delete_context(data_model *);
-
-void
-next_target();
-
-void
-get_tags();
+using forma::make_context;
+using forma::make_forma;
+using forma::make_output;
 
 int main(int argc, char* argv[]) {
 /* If true, output the result to output
@@ -48,63 +30,29 @@ bool output = true;
   The container will remove all
   duplicates.
 */
-vector<tag_t> context_tags;
+vector<tag_t> context_tag;
+vector<target_t> target;
 
-context_t context (make_context());
-
-  if (!context)
-//  log<true>(fatal, ) << "cout not create context database.";
-  return 1;
-
+auto context (make_contxet());
 // Retrieve all context tags
-get_tags();
-copy(
-  idata_model_iterator<tag_t>(
-    * context.get()
-  )
-, idata_model_iterator<tag_t>()
-, back_inserter(context_tags)
+sync(context);
+copy (
+  cast_iterator<tag_t>(begin(context))
+, cast_iterator<tag_t>(end())
+, back_inserter(context_tag)
 );
 
-database_t db (make_formadb());
-  if (!db)
-  return 1;
+auto db (make_forma()));
+sync(db);
+for (auto const& dependecy : db){
+  if (find (begin(context_tag), end(context_tag), dependecy) != end(context_tag))
+  copy(begin(db), end(db), back_inserter(target));
+next_target(db);
+sync(db);
+}
 
-next_target();
-idata_model_iterator<target_t>
-  target_end, target_iter(db);
-
-  if (target_end == target_iter)
-  return 1;
+  if (output == false) return 0;
+auto make (make_output());
 
 return 0;
-}
-
-data_model *
-make_context(){
-data_model * db = new data_pattern::sqlite("onion.sqlite");
-  if (! db->set_iwriter<tag_iwriter>())
-  throw "iwriter error";
-return db;
-}
-
-data_model *
-make_formadb(){
-data_model * db = new data_pattern::sqlite("onion.sqlite");
-  if (! db->set_iwriter<tag_iwriter>())
-  throw "iwriter error";
-return db;
-}
-
-void
-get_tags(){
-}
-
-void
-next_target(){
-}
-
-void
-delete_context(data_model * _db){
-delete _db;
 }
